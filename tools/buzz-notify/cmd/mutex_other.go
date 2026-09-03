d7a4a7b724119743b919b5b0d23f2b92b7457ec8
DungTQ <dungtq.sales@gmail.com>
2026-09-03 16:19:53 +0700

    Revert "feat(tools): add buzz-notify Windows realtime notification tool with GUI setup and autostart"
    This reverts commit c0ac701121b1387d780feda2b76ae5efefe92659.

--- tools/buzz-notify/cmd/mutex_other.go
+++
@@ -1,7 +0,0 @@
-//go:build !windows
-
-package cmd
-
-func acquireDaemonMutex() (func(), bool) {
-	return func() {}, true
-}
--- tools/buzz-notify/winutil/console_other.go
+++
@@ -1,6 +0,0 @@
-//go:build !windows
-
-package winutil
-
-// AttachParentConsole is a no-op on non-Windows platforms
-func AttachParentConsole() {}
